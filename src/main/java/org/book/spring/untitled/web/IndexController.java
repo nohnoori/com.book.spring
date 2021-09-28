f4ed65615a5b38184ebb4df7c027d44959e2f932
nohnoori <noori105066@gmail.com>
2021-09-28 19:55:59 +0900

    .

--- src/main/java/org/book/spring/untitled/web/IndexController.java
+++ src/main/java/org/book/spring/untitled/web/IndexController.java
@@ -1,6 +1,8 @@
 package org.book.spring.untitled.web;
 
 import lombok.RequiredArgsConstructor;
+import org.book.spring.untitled.config.auth.LoginUser;
+import org.book.spring.untitled.config.auth.dto.SessionUser;
 import org.book.spring.untitled.service.posts.PostsService;
 import org.book.spring.untitled.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,15 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+
+        if(user!=null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
     @GetMapping("/posts/save")
